e9b5f493cbdd12907d9e44c8097a4827279b5f42
taddeimania <jtaddei@gmail.com>
2014-09-26 09:38:34 -0500

    Version: bump to 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='generic-request-signer',
-    version='0.2.3',
+    version='0.3.0',
     author='imtapps',
     url='https://github.com/imtapps/generic-request-signer',
     description="A python library for signing http requests.",
