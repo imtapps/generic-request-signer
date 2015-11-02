ee33238d1ae98968f6f8fdcaada3cffc73b08c92
TayHobbs <hobbstay@gmail.com>
2015-11-02 12:13:40 -0600

    Version: 0.4.0 ~> 1.0.0

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-
 from setuptools import setup, find_packages
 
 setup(
     name='generic-request-signer',
-    version='0.4.0',
+    version='1.0.0',
     author='imtapps',
     url='https://github.com/imtapps/generic-request-signer',
     description="A python library for signing http requests.",
