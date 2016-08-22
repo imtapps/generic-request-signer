134b750187fe375a856027688a08189c2bfd2d73
voidnologo <(none)>
2016-08-22 12:25:36 -0500

    Bug Fix: add 'message' attribute to WebException

--- generic_request_signer/exceptions.py
+++ generic_request_signer/exceptions.py
@@ -1,7 +1,17 @@
+import six
+
+
 class WebException(Exception):
     """
     Base Exception for client errors
     """
+
+    def __init__(self, message):
+        self.message = message
+        if six.PY3:
+            super().__init__()
+        else:
+            super(WebException, self).__init__()
 
 
 class HttpMethodNotAllowed(Exception):
--- generic_request_signer/tests/exceptions_tests.py
+++ generic_request_signer/tests/exceptions_tests.py
@@ -8,5 +8,11 @@ class ExceptionsTests(unittest.TestCase):
     def test_web_exception_is_subclass_of_exception(self):
         self.assertTrue(issubclass(WebException, Exception))
 
+    def test_web_exception_has_message(self):
+        e = WebException('x')
+        with self.assertRaises(AssertionError):
+            with self.assertRaises(AttributeError):
+                e.message
+
     def test_http_method_not_allowed_is_subclass_of_exception(self):
         self.assertTrue(issubclass(HttpMethodNotAllowed, Exception))
