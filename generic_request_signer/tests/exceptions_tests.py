ac6cedde2475c82fee7f2c2836b89859b4a022e7
voidnologo <(none)>
2016-08-22 15:16:40 -0500

    Test: remove unneeded assertions

--- generic_request_signer/tests/exceptions_tests.py
+++ generic_request_signer/tests/exceptions_tests.py
@@ -10,16 +10,10 @@ class ExceptionsTests(unittest.TestCase):
 
     def test_web_exception_has_message(self):
         e = WebException('x')
-        with self.assertRaises(AssertionError):
-            with self.assertRaises(AttributeError):
-                e.message
         self.assertEqual(e.message, 'x')
 
     def test_web_exception_does_not_error_if_no_message_passed_in(self):
         e = WebException()
-        with self.assertRaises(AssertionError):
-            with self.assertRaises(AttributeError):
-                e.message
         self.assertEqual(e.message, '')
 
     def test_http_method_not_allowed_is_subclass_of_exception(self):
