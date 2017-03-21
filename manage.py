f676d49fd604ebd5b223d2f4d78b9e829c995afe
mattjmorrison <mattjmorrison@mattjmorrison.com>
2017-03-21 10:53:35 -0500

    Peripheral: Added manage.py so this works like a django project

---
+++ manage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+import sys
+from nose.core import run
+
+
+if __name__ == '__main__':
+    # This is a hack to get around the fact that this project does not use Django
+    sys.argv = sys.argv[:1] + sys.argv[2:]
+    run(*[], **{})
